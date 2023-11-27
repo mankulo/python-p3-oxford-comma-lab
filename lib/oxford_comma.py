4343973b514e61a3ed7e51a58b15503429dee5b3
Abdisemed Abdullahi <abdisemed20@gmail.com>
2023-11-28 01:03:56 +0300

    completed

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,11 +1,11 @@
-def oxford_comma(arr):
+def oxford_comma(lst):
     if not str:
         return ""
-    elif len(arr)==1:
-        return arr[0]
-    elif len(arr)==2:
-        return " and ".join(arr)
+    elif len(lst)==1:
+        return lst[0]
+    elif len(lst)==2:
+        return " and ".join(lst)
     else:
-        return ", ".join(arr[:-1])+ f", and {arr[-1]}"
+        return ", ".join(lst[:-1])+ f", and {lst[-1]}"
 result = oxford_comma(["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"])
 print(result)
